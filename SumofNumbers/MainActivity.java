d94a8dd6b8953ac713188f2d44070552f1ca4c91
GOKUL SONA <142295118+gokulsona@users.noreply.github.com>
2024-10-16 22:57:57 +0530

    Sum of Two Numbers

---
+++ SumofNumbers/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.sumofnumbers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText number1, number2, result;
+    Button sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        result = findViewById(R.id.result);
+        sum = findViewById(R.id.sum);
+
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double num1 = Double.parseDouble(number1.getText().toString());
+                double num2 = Double.parseDouble(number2.getText().toString());
+                result.setText("Result : "+(num1+num2));
+            }
+        });
+
+
+    }
+}
